03bceca92bda02ad625fec9ad0169e6b5944697b
zaidanahd <ahmadzaidan0512@gmail.com>
2026-04-02 16:42:48 +0700

    no2dan3

--- Latihan/src/Karyawan.java
+++ Latihan/src/Karyawan.java
@@ -1,6 +1,8 @@
 public class Karyawan {
-    private String idKaryawan;
-    private String nama;
+    private String idKaryawan;//buat no 1
+    private String nama;//buat no 1
+    private double gajiPokok;//buat no 2
+    private String departemen;//buat no 3
 
     public Karyawan (String idKaryawan, String nama){
         this.idKaryawan = idKaryawan;
@@ -15,11 +17,33 @@ public class Karyawan {
         return nama;
     }
 
+    public double getgajiPokok(){
+        return gajiPokok;
+    }
+
     public void setKaryawan(String idKaryawan){
         this.idKaryawan = idKaryawan;
     }
 
     public void setnama(String nama){
         this.nama = nama;
+    }
+
+    public void setgajiPokok(double gajiPokokAwal){
+        
+        if (gajiPokokAwal >= 0) {
+            this.gajiPokok = gajiPokokAwal;
+        } else {
+            this.gajiPokok = 0;
+            System.out.println("Saldo awal gaboleh negatif.");
+        }
+    }
+
+    public void settampilkanDataKaryawan(){
+        
+        System.out.println("123456");
+        System.out.println("zaidan");
+        System.out.println("5000000");
+        System.out.println("AcademyUP");
     }
 }
